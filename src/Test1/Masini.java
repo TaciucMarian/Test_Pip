2da86bbeb3fd5064944d481745ef3164a40da017
Taciuc Marian <liviu-marian.taciuc@stundent.tuiasi.ro>
2021-03-12 08:28:09 +0200

    Adaugare Comentarii

--- src/Test1/Masini.java
+++ src/Test1/Masini.java
@@ -1,10 +1,21 @@
 package Test1;
 
+
+/**
+ * Dezvoltata pentru informatii <b>masina</b>
+ * @author Taciuc Marian
+ *
+ */
 public class Masini {
 	int an_productie;
 	String culoare;
 	String marca;
-	
+	/**
+	 * Constructor Masina cu informatii de baza
+	 * @param an sa fie intre 2010-2021
+	 * @param cul  culoare in format string
+	 * @param marca  marca autorizata
+	 */
 	Masini(int an, String cul, String marca){
 		an_productie=an;
 		culoare=cul;
@@ -14,6 +25,22 @@ public class Masini {
 	
 	void Afisare(){
 		System.out.println("Masina maraca: " + marca + " produsa in anul: " + an_productie + " este de culoare: " + culoare);
+	}
+	/**
+	 * Printeaza infromatii masian in forma neautorizata
+	 * @param flag 0 -- daca se printeaza normal </br>
+	 * 		  flag 1 -- forma neautorizata
+	 * @return true - printa reusita
+	 * @throws Exception - cand nu exista toate informatiile despre masina
+	 */
+	boolean print(int flag)throws Exception{
+		int x =100;
+		
+		if (flag==0){
+			throw new Exception();
+		}
+		
+		return true;
 	}
 	
 	
--- src/Test1/Test.java
+++ src/Test1/Test.java
@@ -8,7 +8,7 @@ class Raspuns{
 }
 
 public class Test {
-	public static void main(String[] args){
+	public static void main(String[] args) throws Exception{
 		
 		System.out.println("Primul test");
 		System.out.println("Que tal?");
@@ -18,7 +18,11 @@ public class Test {
 		System.out.println("Imi pare rau ca nu am raspuns mai devreme");
 		
 		Masini a=new Masini(2012, "albastra", "Volvo");
+		Masini b=new Masini(2021, "negru", "BMW");
+		
+		
 		a.Afisare();
+		b.print(0);
 		
 	}
 
